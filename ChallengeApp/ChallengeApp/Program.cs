b440148f509b63ca3b1648dcae658a69b3911ae3
Olgierd73 <maclawryn@vp.pl>
2023-01-20 12:06:00 +0100

    zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zadanie domowe dzień 4
+string name = "Ewa";
+var age = 33;
+bool isFemale = true;
+
+if (isFemale)
+
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa,lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30");
+    }
+}
+else
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Mężczyzna pełnoletni");
+    }
+    else 
+    {
+        Console.WriteLine("Mężczyzna niepełnoletni");
+    }
+}
+        
+
+
+
+
