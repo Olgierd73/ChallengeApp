0f8149930c255df6a6f15e828eca39536f5b33b9
Olgierd73 <maclawryn@vp.pl>
2023-01-24 11:32:02 +0100

    zadanie domowe z dn 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,30 +1,23 @@
-﻿int number = 770912734;
+﻿int number = 777907777;
 string numberInString = number.ToString();
 char[] letters = numberInString.ToArray();
 
-List<char> typeOfNumbers = new List<char>();
-typeOfNumbers.Add('0');
-typeOfNumbers.Add('1');
-typeOfNumbers.Add('2');
-typeOfNumbers.Add('3');
-typeOfNumbers.Add('4');
-typeOfNumbers.Add('5'); 
-typeOfNumbers.Add('6');
-typeOfNumbers.Add('7');
-typeOfNumbers.Add('8');
-typeOfNumbers.Add('9');
+List<char> typeOfNumbers = new List<char>
+{'0','1','2','3','4','5','6','7','8','9'};
 
 Console.WriteLine(number);
 
-foreach(var cypfer in typeOfNumbers) 
+foreach(var cypfer in typeOfNumbers)
 {
     int count = 0;
      foreach (var s in letters)
     {
-        if (cypfer == s) count++;          
+        if (cypfer == s)
+        {
+            count++;
+        }
     }
              Console.WriteLine(cypfer + "=>" + count);
 }   
-
 
    
