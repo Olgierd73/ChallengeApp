93631d09d549b810e6e0d9aef6684e4db82ca35f
Olgierd73 <maclawryn@vp.pl>
2023-01-25 13:40:43 +0100

    zadanie domowe podejście 2

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,22 @@
-﻿int number = 777907777;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿int number = 1000000000;
+string numberInString = number.ToString();//liczbę zamienia na symbole literowe na nią się składające
+char[] letters = numberInString.ToArray();// tworzy tablice z symboli -cyfr -tworzących liczbe -number
 
-List<char> typeOfNumbers = new List<char>
+List<char> typeOfNumbers = new List<char> // określa szuflady  na liście rozdzielonych na cyfry liczb
 {'0','1','2','3','4','5','6','7','8','9'};
 
-Console.WriteLine(number);
+Console.WriteLine(number);// pokazuje badaną liczbę
 
-foreach(var cypfer in typeOfNumbers)
+foreach(var cypfer in typeOfNumbers)  //tworzy pętlę z listy podanych cyfr
 {
-    int count = 0;
-     foreach (var s in letters)
-    {
-        if (cypfer == s)
+    int count = 0;                 // pętla startuje od 0 
+    foreach (var s in letters)     //tworzy drugą pętle z tablicy cyfr
+    {                               //z listy okreslonej warunkiem if
+        if (cypfer == s)             // "jeśli" wśród  listy cyfr sa takie same 
         {
-            count++;
+            count++;             // "to" wynikiem jest s + 1, pokazuje ilości takich samych cyfr w danej liczbie np dwa 0 to 0+1+1
         }
     }
-             Console.WriteLine(cypfer + "=>" + count);
-}   
+             Console.WriteLine(cypfer + "=>" + count); //pokazuje pętlę pierwsza (rozłozone na znaki cyfry) i drugą (
+}
 
-   
